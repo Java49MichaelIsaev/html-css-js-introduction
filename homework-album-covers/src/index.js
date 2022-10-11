0b96864ac23c1fc0d3899014093f49d0cbecb906
mdisaev <mdisaev@gmail.com>
2022-10-11 04:35:37 +0300

    Added external link

--- homework-album-covers/src/index.js
+++ homework-album-covers/src/index.js
@@ -2,13 +2,23 @@
  const anchors = document.querySelectorAll(".thumbnails-anchor");
  const detailsImage = document.querySelector(".details-image");
  const detailsTitle = document.querySelector(".details-title");
+ printSomething();
+
  function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
  }
- for(let i = 0; i < anchors.length; i++){
+ for(let i = 0; i < anchors.length; i++)
+ {
     anchors[i].addEventListener("click", function(){
         console.log("event - click on ", anchors[i]); 
         setDetails(anchors[i]);
     })
+ }
+
+ function printSomething(){
+         console.log("anchor's amount is ", anchors.length);
+         console.log("a's amount is ", document.querySelectorAll('a').length);
+         console.log("ul's amount is ", document.querySelectorAll('ul').length);
+         console.log("li's amount is ", document.querySelectorAll('li').length);   
  }

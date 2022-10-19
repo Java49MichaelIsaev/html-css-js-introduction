d9af125b1fb257f3772b84bc49e9beb09dcffd9a
mdisaev <mdisaev@gmail.com>
2022-10-19 06:47:17 +0300

    added logo and background

--- homework-album-covers/src/index.js
+++ homework-album-covers/src/index.js
@@ -1,13 +1,22 @@
+//    window.onload = pageLoadedHandler;
+//    function pageLoadedHandler() { 
+//    alert("I'm alive!");
+// }
+ 
+ 
  console.log("Launching script");
  const anchors = document.querySelectorAll(".thumbnails-anchor");
  const detailsImage = document.querySelector(".details-image");
  const detailsTitle = document.querySelector(".details-title");
+ const detailsAnchor = document.querySelector(".details-anc");
+ const detailsFrame = document.querySelector(".details-frame");
  
  printSomething();
 
  function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+    detailsAnchor.setAttribute("details-url", anchor.getAttribute('data-details-url'))
  }
  for(let i = 0; i < anchors.length; i++)
  {
@@ -18,6 +27,9 @@
     })
  }
 
+ detailsFrame.addEventListener("click", function(){
+   window.open(detailsAnchor.getAttribute('details-url' ))
+ })
 
  function printSomething(){
          console.log("anchor's amount is ", anchors.length);
@@ -25,3 +37,5 @@
          console.log("ul's amount is ", document.querySelectorAll('ul').length);
          console.log("li's amount is ", document.querySelectorAll('li').length);   
  }
+
+

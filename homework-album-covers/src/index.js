f6eb54aa2de02ea44869e0aa4cd7b616423e93bd
mdisaev <mdisaev@gmail.com>
2022-10-25 08:10:13 +0300

    music added - homework 10

--- homework-album-covers/src/index.js
+++ homework-album-covers/src/index.js
@@ -1,9 +1,3 @@
-//    window.onload = pageLoadedHandler;
-//    function pageLoadedHandler() { 
-//    alert("I'm alive!");
-// }
-
-
 console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
@@ -12,45 +6,76 @@ const detailsAnchor = document.querySelector(".details-anc");
 const detailsFrame = document.querySelector(".details-frame");
 const mainClass = document.querySelector(".main-class");
 const detailsContainer = document.querySelector(".details-container");
+const detailsMusic = document.querySelector(".thumbnails-music");
 const HIDDEN = "hidden";
 const IS_POINT = "is-point";
 
 function setDetails(anchor) {
    detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
-   detailsAnchor.setAttribute("details-url", anchor.getAttribute('data-details-url'))
-}
-for (let i = 0; i < anchors.length; i++) {
-   anchors[i].addEventListener("click", function () {
-      console.log("event - click on ", anchors[i]);
-      showDetails();
-      setDetails(anchors[i]);
-
-   })
+   detailsAnchor.setAttribute("details-url", anchor.getAttribute('data-details-url'));
+   detailsMusic.setAttribute('src', anchor.getAttribute('data-details-music'));
 }
 
-//   detailsFrame.addEventListener("click", function(){
-//    window.open(detailsAnchor.getAttribute('details-url' ))
-//   })
+function startMusic(anchor) {
+   detailsMusic.play('src', anchor.getAttribute('data-details-music'));
+   setTimeout(function () {
+      detailsMusic.pause('src', anchor.getAttribute('data-details-music'));
+   }, 5000)
+}
+
 
 function showDetails() {
    mainClass.classList.remove(HIDDEN);
    detailsContainer.classList.add(IS_POINT);
-   setTimeout(function() {
-      detailsContainer.classList.remove(IS_POINT);              
-   },1)
+   setTimeout(function () {
+      detailsContainer.classList.remove(IS_POINT);
+   }, 1)
 }
 
 function hideDetails() {
    mainClass.classList.add(HIDDEN);
 }
 
+detailsFrame.addEventListener("click", function () {
+   window.open(detailsAnchor.getAttribute('details-url'))
+})
+
+for (let i = 0; i < anchors.length; i++) {
+   anchors[i].addEventListener("click", function () {
+      console.log("event - click on ", anchors[i]);
+      showDetails();
+      setDetails(anchors[i]);
+      startMusic(anchors[i]);
+      // stopMusic(anchors[i]);
+
+   })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //  function printSomething(){
 //          console.log("anchor's amount is ", anchors.length);
 //          console.log("a's amount is ", document.querySelectorAll('a').length);
 //          console.log("ul's amount is ", document.querySelectorAll('ul').length);
-//          console.log("li's amount is ", document.querySelectorAll('li').length);}   
+//          console.log("li's amount is ", document.querySelectorAll('li').length);} 
 
 
-
+//    window.onload = pageLoadedHandler;
+//    function pageLoadedHandler() { 
+//    alert("I'm alive!");
+// }

83236e4d320d7416d5750aa8b6bad7ceb17e68a0
mdisaev <mdisaev@gmail.com>
2022-10-31 19:32:57 +0200

    debugger

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-console.log("Hello world!");
+// console.log("Hello world!");
+
 // for(var i = 0; i < 3; i++){
-//     setTimeout(function(){console.log(i)})
-// } 
+//     // setTimeout(function(){
+//         console.log('var', i);
+// // }); 
+// // }
+// }
+// // for(let i = 0; i < 3; i++){
+// //     setTimeout(function(){console.log(i)})
+// // } => 0 1 2
+
+// // for(const i = 0; i < 3; i++){
+// //     console.log(i);`
+
+// // }
+
+// // console.log(i);} }}}}}}}
 
 
-// for(let i = 0; i < 3; i++){
-//     setTimeout(function(){console.log(i)})
-// } => 0 1 2
+function sumDigits(number) {
 
-for(const i = 0; i < 3; i++){
-    console.log(i);
+    let sum = 0;
+    
+
+    if (number < 0){
+        number = -number;
+    }
+        
+    while (number > 0)
+     {
+    console.log("blablabla")
+        sum += number % 10;
+        number = Math.floor(number/10);
+    } 
+        return sum;      
 }
-
-console.log(i);
+     console.log('sum=', sumDigits(123));

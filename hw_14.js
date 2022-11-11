4d6e57e32c179b13c9b8ae38c99ab5c1be3266ac
mdisaev <mdisaev@gmail.com>
2022-11-11 02:51:01 +0200

    hw_14

--- hw_14.js
+++ hw_14.js
@@ -1,63 +1,93 @@
-function ulSurround(array) {
+// function ulSurround(array) {
+    
+//     let array1 = array.map(function (e) { return "<li>" + e + "</li>" });
+//     array1.splice(0, 0, "<ul>")
+//     array1.splice(array1.length, 0, "</ul>")
+//     console.log(array1);
+// }
 
-    array1 = array.map(function (e) { return "<li>" + e + "</li>" });
-    array1.splice(0, 0, "</ul>")
-    array1.splice(array1.length, 0, "</ul>")
-    console.log(array1);
-}
-ulSurround(["abc", "lmn", "cd"])
-ulSurround(["queen", "ramones", "kino"])
-ulSurround([1, 2, 3])
+//return array!!!
 
-
-
-function count(array, element) {
-    let counter = 0;
-    array.map(function (e) {
-        if (e == element)
-            counter++;
-    })
-    console.log(counter);
-}
-count(["abc", "lmn", "cd", "abc", "abc"], "cd")
-count(["to", "be", "or", "not", "to", "be"], "be")
+// ulSurround(["abc", "lmn", "cd"])
+// ulSurround(["queen", "ramones", "kino"])
+// ulSurround([1, 2, 3])
 
 
-function arrayCopy(src, srcPos, dst, dstPos, length) {
+// let strings=["abc", "lmn", "cd", "abc", "abc"]
 
-    let array = src.slice()
+// console.log(count(strings, "cd"));
 
-    let numbers = array.splice(srcPos, length)
+// function count(array, sample) {
+//         return strings.reduce(function(count, element) {
+//         return element === sample ? count + 1 : count;
+        
+//     },0)
+// }
 
-    for (let index = 0; index < numbers.length; index++) {
-        dst.splice(dstPos + index, 0, numbers[index])
-    }
-    console.log(dst)
-}
-
-arrayCopy([1, 2, 3, 4, 5, 6, 7], 3, [10, 20, 30, 40, 50, 60, 70], 4, 3)
-arrayCopy([1, 2, 3, 4, 5, 6, 7, 8], 6, [10, 20, 30, 40, 50, 60, 70], 0, 2)
-arrayCopy([99, 88, 0, 17, -15, 10], 2, [0, -5, 3, 10, 15, 20, 15], 4, 2)
+// count(["abc", "lmn", "cd", "abc", "abc"], "cd")
+// count(["to", "be", "or", "not", "to", "be"], "be")
 
 
-function move(array, index, offset) {
+// function arrayCopy(src, srcPos, dst, dstPos, length) {
 
-    let a = array.splice(index, 1);
-    let array1 = array.slice();
+//     let array = src.slice()
 
-    if (offset > 0) {
-        array1.splice(index + offset, 0, a[0])
-    }
-    else {
-        array1.splice(offset - (-index), 0, a[0])
-    }
+//     let numbers = array.splice(srcPos, length)
+//     // V.R. Try to do the same without for
+//     for (let index = 0; index < numbers.length; index++) {
+//         dst.splice(dstPos + index, 0, numbers[index])
+//     }
+//     console.log(dst)
+// }
 
-    console.log(array1)
-}
+// arrayCopy([1, 2, 3, 4, 5, 6, 7], 3, [10, 20, 30, 40, 50, 60, 70], 4, 3)
+// arrayCopy([1, 2, 3, 4, 5, 6, 7, 8], 6, [10, 20, 30, 40, 50, 60, 70], 0, 2)
+// arrayCopy([99, 88, 0, 17, -15, 10], 2, [0, -5, 3, 10, 15, 20, 15], 4, 2)
 
-move([1, 2, 3, 4, 5, 6, 7], 3, -1)
-move([1, 2, 3, 4, 5, 6, 7], 2, 4)
-move([1, 2, 3, 4, 5, 6, 7], 4, -1)
-move([1, 2, 3, 4, 5, 6, 7], 6, -1)
-move([5, 5, 5, 4, 5, 5, 5], 3, -2)
-move([5, 5, 5, 4, 5, 5, 5], 3, 2)
+
+// function move(array, index, offset) {
+
+//     let a = array.splice(index, 1);
+//     let array1 = array.slice();
+//     array1.splice(index + offset, 0, a[0])
+//     return array1;
+// }
+
+
+// console.log(move([1, 2, 3, 4, 5, 6, 7], 3, -1))
+
+// move([1, 2, 3, 4, 5, 6, 7], 2, 4)
+// move([1, 2, 3, 4, 5, 6, 7], 4, -1)
+// move([1, 2, 3, 4, 5, 6, 7], 6, -1)
+// move([5, 5, 5, 4, 5, 5, 5], 3, -2)
+// move([5, 5, 5, 4, 5, 5, 5], 3, 2)
+
+
+// function arrayCopy(src, srcPos, dst, dstPos, length) {
+//     let arForCopy = src.slice(srcPos, srcPos + length);
+//     console.log(arForCopy);
+//     dst.splice(dstPos, 0, arForCopy);
+// }
+
+// let ar1 = [1,2,3,4,5,6];
+// let ar2 = [10,30,40,50];
+// arrayCopy(ar1,2,ar2,1,3);
+// console.log(`ar1: ${ar1}, ar2: ${ar2} arrayCopy(ar1,2,ar2,1,3): ${ar2}`)
+
+// function getEvenNumbers(numbers) {
+//     let res = numbers.filter(n => n % 2 == 0);
+//     return res;
+// }
+
+
+// console.log(getEvenNumbers([989, 4,444]))
+
+// let ar=[ 2,9, 3, 1999];
+// ar.sort((a, b) => a-b);
+
+// console.log(ar);
+// ar = ["abc", "a", "lmn", "y"];
+// ar.sort((a, b) => a < b ? -1 : 1);
+// console.log(ar);
+// ar.sort((a, b) => a.length - b.length);
+// console.log(ar);

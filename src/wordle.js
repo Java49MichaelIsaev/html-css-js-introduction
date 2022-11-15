1c70f80a4c1d3d2d7e720e040edd2c95593e8364
mdisaev <mdisaev@gmail.com>
2022-11-15 02:35:50 +0200

    wordle_homework

--- src/wordle.js
+++ src/wordle.js
@@ -1,35 +1,55 @@
+let count = 6;
 const word = "table"
 const N_LETTERS = 5;
 const letterElements = document.querySelectorAll(".letter-guess")
+
 function onChange(event) {
     const wordGuess = event.target.value;
-    event.target.value='';
+    event.target.value = '';
+    count = count - 1;
+    if (word !== wordGuess) {
+        gameOver(count);
+    }
+
     if (wordGuess.length != N_LETTERS) {
-    alert(`A word should contain ${N_LETTERS} letters`) ;
+        alert(`A word should contain ${N_LETTERS} letters`);
+
     } else {
-        let count = 0;
-        count++;
-        if (count > 6){ 
-            
-        }
         const wordAr = Array.from(wordGuess);
         wordAr.forEach((l, i) => letterElements[i].innerHTML = l)
+        if (word == wordGuess) {
+            youWin()
+        }
         const colors = wordAr.map((l, i) => {
+
             let index = word.indexOf(l); //есть ли эта буква в слове - если есть возвращает индекс этой буквы в массиве, если нет - -1
             let res = 'red';
+
             if (index > -1) {
                 res = index == i ? 'green' : 'yellow'
             }
             return res;
-
-
         })
-        colors.forEach((c, i) => 
-        letterElements[i].style.color=c) 
 
-
+        colors.forEach((c, i) =>
+            letterElements[i].style.color = c)
     }
 }
 
-// количество ходов, если больше шести ходов - (введенно слово == задуманное слова) выводим лэйблом = сначала пустой = потом в лейбл помещаем конгратулэйшнс и количество ходов - или сорри ю р лост
-//  в дом элемент поставить свойства на каждый ход количество уменьшается до нуля
+function gameOver(count) {
+
+    var x = document.getElementById("game-over");
+    if (count > 1) {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function youWin() {
+
+    var x = document.getElementById("game-win");
+    x.style.display = "block";
+}
+
+

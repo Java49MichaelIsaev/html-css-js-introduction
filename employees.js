38e95b5bf5544120d61f3915df03bf567fb29dab
mdisaev <mdisaev@gmail.com>
2022-11-25 07:03:38 +0200

    second version

--- employees.js
+++ employees.js
@@ -7,7 +7,7 @@ function createRandomEmployees(nEmployees, idDigits, minSalary, maxSalary) {
     numberOfEmployees = nEmployees;
 
     for (i = 0; i < nEmployees; i++) {
-        emp = { name: `Name: ${randomN()} - ID : ${randomIdDigits(idDigits)}`, year: randomA(), salary: randomS(minSalary, maxSalary) }
+        emp = { name: `Name: ${randomName()} - ID : ${randomIdDigits(idDigits)}`, year: randomAge(), salary: randomSalary(minSalary, maxSalary) }
         result.push(emp);
     }
     return result;
@@ -18,6 +18,7 @@ function getAverageAge(result) {
     let averageAge = result.reduce((accumulator, currentValue) =>
         ((new Date().getFullYear() - currentValue["year"]) + accumulator),
         0);
+
     averageAge = Math.floor((averageAge) / numberOfEmployees)
     console.log("Avarage age: " + averageAge)
     return averageAge;
@@ -27,12 +28,13 @@ function getAverageAge(result) {
 function getEmployeeBySalary(result, salaryFrom, salaryTo) {
 
     let result1 = result.filter(result => result["salary"] > salaryFrom && result["salary"] < salaryTo)
-    
+
     result1.sort(function (a, b) {
         return a.salary - b.salary;
     });
-
     console.log(result1)
+    return result1;
+
 }
 
 
@@ -40,15 +42,15 @@ function randomIdDigits(digits) {
     return Math.random().toFixed(digits).split('.')[1];
 }
 
-function randomA() {
+function randomAge() {
     return Math.floor(Math.random() * (2000 - 1940 + 1)) + 1940;
 }
 
-function randomS(min, max) {
+function randomSalary(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomN() {
+function randomName() {
     var result = '';
     var characters = 'abcdefghijklmnopqrstuvwxyz';
     var charactersLength = characters.length;
@@ -59,26 +61,26 @@ function randomN() {
 }
 
 
-console.log(createRandomEmployees(4, 4, 1000, 2000));
-getEmployeeBySalary(result, 1000, 2000)
-getAverageAge(result)
+// console.log(createRandomEmployees(5, 4, 1000, 2000));
+// getEmployeeBySalary(result, 1000, 2000)
+// getAverageAge(result)
 
 
-console.log(createRandomEmployees(1, 1, 100, 1000));
+console.log(createRandomEmployees(20, 3, 100, 1000));
 getEmployeeBySalary(result, 100, 1000)
 getAverageAge(result)
 
-console.log(createRandomEmployees(10, 10, 99999, 6000));
-getEmployeeBySalary(result, 99999, 6000)
-getAverageAge(result)
+// console.log(createRandomEmployees(10, 10, 99999, 6000));
+// getEmployeeBySalary(result, 99999, 6000)
+// getAverageAge(result)
 
-console.log(createRandomEmployees(2, 2, 3000, 6000));
-getEmployeeBySalary(result, 3000, 6000)
-getAverageAge(result)
+// console.log(createRandomEmployees(2, 2, 3000, 6000));
+// getEmployeeBySalary(result, 3000, 6000)
+// getAverageAge(result)
 
 
-createRandomEmployees(4, 4, 1000, 6000)
-getEmployeeBySalary(result, 1000, 3000)
-getAverageAge(result)
+// createRandomEmployees(4, 4, 1000, 6000)
+// getEmployeeBySalary(result, 1000, 3000)
+// getAverageAge(result)
 
 
